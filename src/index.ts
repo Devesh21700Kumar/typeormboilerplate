60686da13228750d9855318c52b41ef145ea15ce
Devesh <rockdev1234560@gmail.com>
2021-02-11 16:46:50 +0530

    init commit

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import express, {Request,Response} from 'express';
+
+import {User} from "./entity/User";
+
+const app = express();
+app.use(express.json());
+
+//Create
+app.post('/users',async (req: Request, res: Response)=>{
+    const {name,email,role} = req.body;
+
+    try{
+        const user = User.create({name,email,role});
+        await user.save();
+        return res.status(200).json(user);
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json(err);
+    }
+
+})
+
+createConnection().then(async (connection) => {
+
+    /*console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
+
+    console.log("Loading users from the database...");
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
+
+    console.log("Here you can setup and run express/koa/any other framework.");*/
+
+    app.listen(5000,()=>{
+        console.log('running at 5000');
+    })
+
+}).catch(error => console.log(error));
