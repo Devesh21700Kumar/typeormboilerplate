cb5e2668ddfe67b5f818ff4b1f4dd1ce46684703
Devesh <rockdev1234560@gmail.com>
2021-02-11 17:29:04 +0530

    uuid generation

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,6 +1,7 @@
 import { Update } from "@material-ui/icons";
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, CreateDateColumn, UpdateDateColumn, BeforeInsert} from "typeorm";
 //psql -U postgres for username postgres
+import {v4 as uuid}  from 'uuid';
 
 @Entity('users')
 export class User extends BaseEntity{
@@ -17,11 +18,21 @@ export class User extends BaseEntity{
     @Column()
     role: string;
 
+    //3rd party package uuid
+    //we'l use listeners for handling uuid
+    @Column({type:'uuid'})
+    uuid: string;
+
     //all beginning with @ are declarators
     @CreateDateColumn()
     createdAt: Date;
 
     @UpdateDateColumn()
     updatedAt: Date;
+
+    @BeforeInsert()
+        createUuid(){
+            this.uuid = uuid();
+        }
 
 }
